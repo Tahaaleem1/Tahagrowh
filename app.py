ea326bc25caa89eec5418b73cb0f7de66e877581
Taha <156198811+Tahaaleem1@users.noreply.github.com>
2025-02-27 12:09:45 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+#streamlit
+import streamlit as st 
+
+st.set_page_config(page_title= "growth mindset project", project_icon="★")
+st.title("Growth Mindset Challenge: Web App with Streamlit")
+
+st.header("🚀 Welcome to Your Growth Journey!")
+st.write("Embrace challenges, learn from mistakes, and unlock your full potential. This AI-powered app helps you build a growth mindset with reflaction, challenges, and achievements! 🌟")
+
+#quote section
+st.header("Today's Growth Mindset Quote")
+st.write('"Success is not final, failure is not fatal: it is the courage to continue that counts." - Winston Churchill')
+
+st.header("What's Your Challenge Today?")
+user_input = st.text_input("Describe a challenge you're facing")
+
+#condition
+if user_input:
+    st.success(f"💪you're facing: {user_input}. keep pushing forword towords your goal! 🚀")
+else:
+    st.warning("Tell us about your challenge to get started!")
+
+#reflexing
+st.header("Reflect on Your Learning")
+reflection = st.text_area("Write your reflections here:")
+
+if reflection:
+    st.success(f"Great Insight! Your reflection: {reflection}")
+ 
+else:
+    st.info("Reflacting on past experience help you grow! Share your difficuities")
+
+#acheivments
+st.header("🏆 Celebrate Your Wins!")
+acheivment = st.text_input("Share someting you've recently accomplished:")
+
+if acheivment:
+    st.success(f"💫 Amazing! You acieved: {acheivment}")
+else:
+    st.info("Big or small , every acheivement counts! Share one now! 😍")
+
+#footer
+st.write("- - -")
+st.write("🚀 Keep believing in yourself. Growth is a journey, not a destination! 🌟")
+st.write("**⛔ Created by Taha Aleem**")
+ 
